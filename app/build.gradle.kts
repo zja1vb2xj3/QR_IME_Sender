165795a70c1f733b7477ff510ad955ba45fad03b
zja1vb2xj3 <zja1vb2xj3@naver.com>
2026-04-29 13:36:27 +0900

    versionCode 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.contrastcre.qr_ime_sender"
         minSdk = 23
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
